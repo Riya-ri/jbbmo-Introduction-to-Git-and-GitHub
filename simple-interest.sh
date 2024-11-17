0ed60e137c646b61887fdd6f102686ebd8aab7b5
Riya-ri <149425775+Riya-ri@users.noreply.github.com>
2024-11-17 14:37:28 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,3 +1,4 @@
+# Here is new update in formula of SI by <riya-ri> 
 #!/bin/bash
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
@@ -12,15 +13,15 @@
 # r, annual rate of interest
 
 # Output:
-# simple interest = p*t*r
+# simple interest = (p*r*t)/100
 
-echo "Enter the principal:"
+echo "Enter the principal amount:"
 read p
 echo "Enter rate of interest per year:"
 read r
 echo "Enter time period in years:"
 read t
 
-s=$(expr $p \* $t \* $r / 100)
+s=$(expr $p \* $r \* $t / 100)
 echo "The simple interest is: "
 echo $s
